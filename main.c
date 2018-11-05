ab6cee542bdee12458e500c84c53f52f04ef2f81
Emily Ferretti <emfer19@users.noreply.github.com>
2018-11-04 20:37:36 -0600

    Update main.c
    updating main

--- main.c
+++ main.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
 
+// Declare action parameters
+#define ACTION_GENERATE_KEY "-g"
+#define ACTION_ENCRYPT "-e"
+#define ACTION_DECRYPT "-d"
+
 
 int main (int argc, char* argv[]) {
 
-    if (argc < 4)
-        printf("Please provide the arguments as follows:\n./DES <mode encrypt decrypt> <key Y or N> <text file> [key file]");
+    if (argc =! 4){
+        printf("Please provide the arguments as follows:\n./DES <mode encrypt decrypt> <text file> <key>");
+        return 1;
+    }
+    else {
+        // Declare file handlers
+        FILE *input_file, *output_file;
+        
+        input_file=fopen(argv[2], "rb");
+        if (!input_file){
+            printf("Error opening input file to read data.");
+            return 1;
+        }
 
+        output_file = fopen(argv[4], "wb");
+		if (!output_file) {
+			printf("Could not open output file to write data.");
+			return 1;
+		}
+
+        if (argv[1] == ACTION_ENCRYPT){
+            encrypt()
+        }
+
+        fclose(input_file);
+    }
     return 0;
 }
