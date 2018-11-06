aa2b24674766142e58927c0eb256efc544e064be
ispylbutton <buttonla@slu.edu>
2018-11-06 11:36:46 -0600

    Delete DES2.h

--- DES2.h
+++
@@ -1,9 +0,0 @@
-void bitwiseAddition(uint64_t from, uint64_t * block, int initPosition, int finPosition);
-
-void permuteBlock(uint64_t * block, bool isInitialPermutation);
-
-uint64_t XOR(uint64_t a, uint64_t b);
-
-void keyScheduler(uint64_t * initKey, uint64_t * nextKey, int roundNum);
-
-void singleRoundDES(uint64_t * leftBlock, uint64_t key);
